79f4122d8bd965205701acf13802760f4f2fc5c4
Robert Whitehead <stuff@rjfw.co.uk>
2015-04-24 13:40:33 +0100

    Upgrading core lib version
    Former-commit-id: e0aa3853b9fe986b4adc44d883c6c53283deb35c [formerly 5b8c380d4b0e27ace2e1925f4407fc29acd6a65e] [formerly 5b8c380d4b0e27ace2e1925f4407fc29acd6a65e [formerly a4a6a5fdee9cfe5832ec0f5c8e5ab217524223c9]] [formerly f1ea446eb27c11dfdd7f7d6b2bb8872fa366ec86 [formerly f1ea446eb27c11dfdd7f7d6b2bb8872fa366ec86 [formerly b0a86bb99530967949e80d457e52bd178b148670]]] [formerly 18179e1a1abf71269bb702d634c4e226a9064335 [formerly 18179e1a1abf71269bb702d634c4e226a9064335 [formerly 5af764e8ed4cb9bd0a5ab7c15948eb644f473072]] [formerly 4d2fe7f8776093c891c2b20bcd6142334f7ae160 [formerly 4d2fe7f8776093c891c2b20bcd6142334f7ae160 [formerly 4d2fe7f8776093c891c2b20bcd6142334f7ae160 [formerly ddd30bdfadb0bdaf3a5b630125f89dde9a9a8ed4]]]]]
    Former-commit-id: 0384fe25f30b8c64b75591931b80f4e6e058869b [formerly 0384fe25f30b8c64b75591931b80f4e6e058869b [formerly 9c94d892ad78fddea4b9feef6c66e69fc04de582]] [formerly 4d2fe7f8776093c891c2b20bcd6142334f7ae160]
    Former-commit-id: d1c43b03d8bba9ac1ee3733d3df4c96928014e11 [formerly d1c43b03d8bba9ac1ee3733d3df4c96928014e11 [formerly d023ef208339f32cc8fbcab160a3492a7edd1964]]
    Former-commit-id: 5259d3dfea4a259f4ed81c4486c41d4eb90e0b3f [formerly 2c044f02c97bf04eafd7314ef020ebf30d7d1c1c]
    Former-commit-id: 56a29774cd328737db28a79c3be9007acf7f3e83
    Former-commit-id: 027042c8f729be4f94946c43b3632dc14942afca

--- Gamelogic/src/main/scala/GameLauncher.scala
+++ Gamelogic/src/main/scala/GameLauncher.scala
@@ -3,19 +3,18 @@ import improbable.corelib.launcher.ShutdownAfterInput
 import improbable.dapi.Launcher
 import improbable.unity.fabric.engine.DownloadableClientEngineDescriptor
 
+import scala.io.StdIn
+
 object ManualEngineSpoolUpGameLauncher extends SuperSeedlingGameLauncher(ManualEngineStartupLaunchConfig) with App with ShutdownAfterInput
 
 object AutoEngineSpoolUpGameLauncher extends SuperSeedlingGameLauncher(AutomaticEngineStartupLaunchConfig) with App with ShutdownAfterInput
 
-object VisibleClient extends DownloadableClientEngineDescriptor(withGui = true)
+object Demonstration extends SuperSeedlingGameLauncher(AutomaticEngineStartupLaunchConfig) with App with ShutdownAfterInput
 
-object Demonstration extends App with ShutdownAfterInput {
-  System.setProperty(DeploymentConfigurationProperties.PROPERTY_IS_PRODUCTION, true.toString)
-  System.setProperty("game_engines_to_start", "VisibleClient@0,0,0")
-  Launcher.startGame(AutomaticEngineStartupLaunchConfig)
-}
+object VisibleClient extends DownloadableClientEngineDescriptor(withGui = true)
 
 class SuperSeedlingGameLauncher(gameSetupSettings: SuperSeedlingLaunchConfig) {
   System.setProperty(DeploymentConfigurationProperties.PROPERTY_IS_PRODUCTION, true.toString)
+  System.setProperty("game_engines_to_start", "VisibleClient@0,0,0")
   Launcher.startGame(gameSetupSettings)
 }
