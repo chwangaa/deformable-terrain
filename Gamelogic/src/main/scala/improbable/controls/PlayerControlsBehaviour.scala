e1ff2d5c7cfe95985d40743b14706523114dd748
rorygreig <rorygreig@gmail.com>
2015-08-14 10:30:50 +0100

    don't inject playercontrolsdata when delegating
    Former-commit-id: 1fd5139e2c41403462482f28a42c8462a64b6d7d [formerly 91a2b5a44fbfdc2870c7ffe59d1d972e4809a923] [formerly 149aad429189fa255de3b36545c3287ae969408f [formerly aa253baecb8ceead2909b3576edceb091dcb49e9]] [formerly 4d9a95e942757678f5ec57f7257ee3dc44b2994a [formerly 6ec1be55c0f3a7e1696f65d59e257f0ed3433bf2] [formerly a7e4c2000ff6c2c0afafca0d68e68322269112d1 [formerly c4faf2c3ed6132296410577f3a0eb93b197be52e]]] [formerly 62021f000eed06fd5f7d343daf2fd28dae7630a4 [formerly 94be33b63611481c09113ac12dcf1e01a745817a] [formerly 2c4e46f82fe4cd56f506617185d8aee6beca644d [formerly 89a9d98f35dc1f5a99abbb852f0f11ec1deb2bf7]] [formerly ba24d8d52c17eda2b65cb84aa354ef9c9e24af4e [formerly 964049cd8501f50243328e5c0ad3dec7b97d5b2a] [formerly c87dbd12c82429155d48f7b95eb62e3905704b82 [formerly c87dbd12c82429155d48f7b95eb62e3905704b82 [formerly b7188dd89851ae52151cd221663720bc5bdae1f6]]]]]
    Former-commit-id: f37a97575c4e3554ab569a84b2dcc5504c8099f1 [formerly b5ab65f8a5007063a5b416df410e575a1c80420e] [formerly bc3d0899f26f1707f3b228544f9575b0ccdf5ace [formerly 79d166d6a060cafe16358fb044097024b5bdd792]] [formerly d73974491c02ee21c529d3faf5b3e90c3e94ddf3 [formerly fad9aa7352b0889b33e109756dda89356e7699eb] [formerly c87dbd12c82429155d48f7b95eb62e3905704b82]]
    Former-commit-id: e8563d4ee64c0e8f5fd26a0fb96ddeaf334c566d [formerly 23047039caf9cb8a00bcedcba2002e0c81c7451e] [formerly f1ee21e2622fc34463bca48cf7118ffe40220626 [formerly b991eb9138e1d57bf5d9ffd275553d12af5a6f61]]
    Former-commit-id: eb335ebd9fd8027d7d98a1ff54a9f2202422b880 [formerly c11976dbe8872c1c367646e3580de98e53b8be87]
    Former-commit-id: 472a79bbed926a720d8c2747ace10dbde48b49f3
    Former-commit-id: d184eb4d7c4032ff759709d6b21ce3fdeb812ed1

--- Gamelogic/src/main/scala/improbable/controls/PlayerControlsBehaviour.scala
+++ Gamelogic/src/main/scala/improbable/controls/PlayerControlsBehaviour.scala
@@ -3,7 +3,7 @@ package improbable.controls
 import improbable.corelib.util.EntityOwnerDelegation.entityOwnerDelegation
 import improbable.papi.entity.{Entity, EntityBehaviour}
 
-class PlayerControlsBehaviour(entity: Entity, playerControlsData: PlayerControlsDataWriter) extends EntityBehaviour {
+class PlayerControlsBehaviour(entity: Entity) extends EntityBehaviour {
 
   entity.delegateStateToOwner[PlayerControlsData]
 
